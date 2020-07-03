6b4b008b471055d8a1f087b09854e9314853c881
Wallison Henrique <wallisonhen@gmail.com>
2020-07-03 17:13:09 -0300

    Updates

---
+++ precache-manifest.09e6f51544b3f8bb0a6bc12f4162d7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6435371534d26f51f65a4d265d3027",
+    "url": "/calculadora-trader/index.html"
+  },
+  {
+    "revision": "d803d12246845372bc55",
+    "url": "/calculadora-trader/static/css/main.c836897d.chunk.css"
+  },
+  {
+    "revision": "884be53b21c73c427ddb",
+    "url": "/calculadora-trader/static/js/2.8c104f67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora-trader/static/js/2.8c104f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d803d12246845372bc55",
+    "url": "/calculadora-trader/static/js/main.105feb01.chunk.js"
+  },
+  {
+    "revision": "073b5dea8940f3838749",
+    "url": "/calculadora-trader/static/js/runtime-main.a7bd202d.js"
+  }
+]);
