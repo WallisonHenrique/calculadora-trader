403e3a21048f5ec4ecb9e3ac6095d47c644b68c5
Wallison Henrique <wallisonhen@gmail.com>
2020-07-03 21:29:19 -0300

    Alteração no título do aplicativo

---
+++ build/precache-manifest.4b964994cc215b7ba6ccb0e5ddbb5cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f6cda83e8e3ee6427e60aea8d5c31d",
+    "url": "/calculadora-trader/index.html"
+  },
+  {
+    "revision": "5e31052a2d3cae4fba33",
+    "url": "/calculadora-trader/static/css/main.c836897d.chunk.css"
+  },
+  {
+    "revision": "884be53b21c73c427ddb",
+    "url": "/calculadora-trader/static/js/2.8c104f67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora-trader/static/js/2.8c104f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e31052a2d3cae4fba33",
+    "url": "/calculadora-trader/static/js/main.80004999.chunk.js"
+  },
+  {
+    "revision": "073b5dea8940f3838749",
+    "url": "/calculadora-trader/static/js/runtime-main.a7bd202d.js"
+  }
+]);
